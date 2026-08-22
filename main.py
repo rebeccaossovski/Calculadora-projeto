1b022d26df394261a1c5f7b7014b9ea1d2bd1627
rebeccaossovski <rebeccaossovski@gmail.com>
2026-08-22 17:45:22 -0300

    API Calculadora
    Adiciona API Calculadora com FastAPI

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "API Calculadora"}
+
+@app.get("/somar/{a}/{b}")
+async def somar(a: int, b: int):
+    resultado = a + b
+    return {"Operação": "soma", "a":a, "b":b, "resultado":resultado}
+
+@app.get("/subtrair/{a}/{b}")
+async def subtrair (a: int, b: int):
+    resultado = a - b
+    return {"Operação": "subtracao", "a":a, "b":b, "resultado":resultado}
+
+@app.get("/multiplicar/{a}/{b}")
+async def multiplicar (a: int, b: int):
+    resultado = a * b
+    return {"Operação": "multiplicacao", "a":a, "b":b, "resultado":resultado}
+
+@app.get("/dividir/{a}/{b}")
+async def dividir (a: int, b: int):
+    if b == 0:
+        return {"erro": "Não existe divisão por 0 (zero)"}
+    resultado = a / b
+    return {"Operação": "divisao", "a":a, "b":b, "resultado":resultado}
+
+@app.get("/aleatorio")
+async def numero_aleatorio():
+    import random
+    return {"numero_aleatorio": random.randint(1, 100)}
